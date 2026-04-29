d2ba11f67c4d4816d2c174ec2cb4ed307db2c437
mhmdshbni <muhamadsahbani057@gmail.com>
2026-04-30 01:46:29 +0700

    portofolio abim new tema angkasa

---
+++ components/sub/hero-content.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/lib/motion";
+
+export const HeroContent = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+    >
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">Fullstack Developer</h1>
+        </motion.div>
+
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-2 mt-6 text-4xl md:text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <div>
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              Hello Everyone
+            </span>
+          </div>
+          <div className="whitespace-nowrap">I'm Muhamad Sahbani</div>
+        </motion.div>
+
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          Seorang Fullstack Developer yang berfokus pada pengembangan website,
+          mobile, dan software. Saya juga memiliki minat dalam desain, sehingga
+          mampu menciptakan aplikasi yang fungsional sekaligus menarik secara
+          visual.
+        </motion.p>
+
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          Learn more
+        </motion.a>
+      </div>
+
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/hero-bg.svg"
+          alt="work icons"
+          height={650}
+          width={650}
+          draggable={false}
+          className="select-none"
+        />
+      </motion.div>
+    </motion.div>
+  );
+};
---
+++ constants/index.ts
@@ -0,0 +1,378 @@
+import { FaYoutube, FaFacebook, FaTelegram } from "react-icons/fa";
+import {
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+export const SKILL_DATA = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    image: "reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Framer Motion",
+    image: "framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Stripe",
+    image: "stripe.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+] as const;
+
+export const SOCIALS = [
+  {
+    name: "Instagram",
+    icon: RxInstagramLogo,
+    link: "https://instagram.com/mhmdshbni_",
+  },
+  {
+    name: "Facebook",
+    icon: FaFacebook,
+    link: "https://facebook.com",
+  },
+  {
+    name: "Twitter",
+    icon: FaTelegram,
+    link: "https://t.me/benzsoulzy",
+  },
+] as const;
+
+export const FRONTEND_SKILL = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Material UI",
+    image: "mui.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    image: "reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const BACKEND_SKILL = [
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express.js",
+    image: "express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    skill_name: "Firebase",
+    image: "firebase.png",
+    width: 55,
+    height: 55,
+  },
+  {
+    skill_name: "PostgreSQL",
+    image: "postgresql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "MySQL",
+    image: "mysql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Prisma",
+    image: "prisma.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Graphql",
+    image: "graphql.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const FULLSTACK_SKILL = [
+  {
+    skill_name: "React Native",
+    image: "reactnative.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Tauri",
+    image: "tauri.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Docker",
+    image: "docker.png",
+    width: 70,
+    height: 70,
+  },
+
+  {
+    skill_name: "Figma",
+    image: "figma.png",
+    width: 50,
+    height: 50,
+  },
+] as const;
+
+export const OTHER_SKILL = [
+  {
+    skill_name: "Go",
+    image: "go.png",
+    width: 60,
+    height: 60,
+  },
+] as const;
+
+export const PROJECTS = [
+  {
+    title: "Sistem Informasi Manajemen Aset 79",
+    description:
+      "Sistem informasi manajemen aset yang dirancang khusus untuk mengelola stok barang elektronik dan berbagai alat lainnya. Sistem ini membantu perusahaan, gudang, toko elektronik, atau instansi dalam mencatat, memonitor, dan mengoptimalkan sirkulasi barang secara real-time.",
+    image: "/projects/projectpkl.png",
+    link: "https://example.com",
+  },
+  {
+    title: "Website Peminjaman Barang di Lab RPL",
+    description:
+      "Sebuah sistem manajemen peminjaman barang berbasis web yang dirancang khusus untuk laboratorium RPL. Memudahkan pengelolaan inventaris, pencatatan peminjaman, serta pengembalian barang secara digital. Proyek ini bertujuan untuk meningkatkan efisiensi dan transparansi dalam sirkulasi barang di lingkungan laboratorium.",
+    image: "/projects/project1.png",
+    link: "https://example.com",
+  },
+  {
+    title: "Website E-commerce Cireng Isi Meledak",
+    description:
+      "Platform e-commerce unik dan menghibur untuk menjual cireng isi meledak dengan konsep pemasaran yang kreatif dan sedikit kocak. Website ini tidak hanya menyediakan katalog produk dan sistem pemesanan, tetapi juga menghadirkan pengalaman berbelanja yang menyenangkan dengan sentuhan interaksi pengguna yang lucu dan tidak membosankan.",
+    image: "/projects/project2.png",
+    link: "https://example.com",
+  },
+  {
+    title: "Game Edukasi Anak - Tebak Hewan",
+    description:
+      'Sebuah permainan edukasi sederhana namun menarik yang mengajak anak-anak menebak nama hewan berdasarkan gambar atau suara. Dirancang dengan antarmuka yang ramah anak dan warna-warna ceria, game ini bertujuan untuk meningkatkan kemampuan kognitif dan pengenalan hewan pada usia dini secara menyenangkan.',
+    image: "/projects/project3.jpg",
+    link: "https://example.com",
+  },
+  {
+    title: "RootFacts - Aplikasi Scan Sayuran",
+    description:
+      'Aplikasi berbasis kamera yang memungkinkan pengguna memindai sayuran untuk mendapatkan informasi lengkap seperti nama sayuran, kandungan gizi, tingkat kesegaran, hingga rekomendasi olahan. Proyek ini menggabungkan teknologi computer vision dan basis data nutrisi untuk membantu masyarakat memilih sayuran yang lebih sehat dan berkualitas.',
+    image: "/projects/project4.jpeg",
+    link: "https://example.com",
+  },
+] as const;
+
+export const FOOTER_DATA = [
+  {
+    title: "Community",
+    data: [
+      {
+        name: "YouTube",
+        icon: FaYoutube,
+        link: "https://youtube.com/@muhamadsahbani15",
+      },
+      {
+        name: "GitHub",
+        icon: RxGithubLogo,
+        link: "https://github.com/mhmdshbni",
+      },
+      {
+        name: "Discord",
+        icon: RxDiscordLogo,
+        link: "https://discord.com",
+      },
+    ],
+  },
+  {
+    title: "Social Media",
+    data: [
+      {
+        name: "Instagram",
+        icon: RxInstagramLogo,
+        link: "https://instagram.com/mhmdshbni_",
+      },
+      {
+        name: "Telegram",
+        icon: RxTwitterLogo,
+        link: "https://t.me/benzsoulzy",
+      },
+      {
+        name: "Linkedin",
+        icon: RxLinkedinLogo,
+        link: "https://www.linkedin.com/in/muhamad-sahbani15",
+      },
+    ],
+  },
+  {
+    title: "About",
+    data: [
+      {
+        name: "Become Sponsor",
+        icon: null,
+        link: "https://youtube.com/@muhamadsahbani15",
+      },
+      {
+        name: "Learning about me",
+        icon: null,
+        link: "https://www.linkedin.com/in/muhamad-sahbani15",
+      },
+      {
+        name: "Contact Me",
+        icon: null,
+        link: "muhamadsahbani057@gmail.com",
+      },
+    ],
+  },
+] as const;
+
+export const NAV_LINKS = [
+  {
+    title: "About me",
+    link: "#about-me",
+  },
+  {
+    title: "Skills",
+    link: "#skills",
+  },
+  {
+    title: "Projects",
+    link: "#projects",
+  },
+] as const;
+
+export const LINKS = {
+  sourceCode: "https://wa.me/+6289601722012",
+};
